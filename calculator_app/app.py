ddb2b4e0080de4fe23a786e4cec0207181a79c8b
hem nithish <hemnithish16@gmail.com>
2025-09-17 15:29:41 +0530

    Update app.py

--- calculator_app/app.py
+++ calculator_app/app.py
@@ -36,4 +36,5 @@ def calculate():
         return render_template('calculator.html', result="Error: Invalid input. Please enter numbers.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
